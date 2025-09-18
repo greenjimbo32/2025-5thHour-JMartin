af3413c566afd857620c4b07026dea1282c8e6e8
Jude Martin <jm129303@student.wpsok.org>
2025-09-18 13:19:43 -0500

    playground

--- playground.py
+++ playground.py
@@ -2,6 +2,11 @@
 #class: 5th hour
 #assighnment: playground
 import random
+
+
+
+
+
 players=["bryson" , "eli" , "aiden" , "alden" , "julian" , "ivan" , "huntar" , "statham" , "gabe"]
 bullets=[0,0,0,0,0,0,0,0,1]
 print(players)
@@ -16,41 +21,11 @@ bullets.remove(next_bullet)
 print(bullets)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import random
+magic_8_ball=["yes","no","maybe","perchance","nuh uh monkey","hell no never in a million years","fine I guess"]
+random.shuffle(magic_8_ball)
+print(magic_8_ball)
+print(random.choice(magic_8_ball))
 
 
 
