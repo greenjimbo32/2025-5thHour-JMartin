1c7e47e4ca65219106a9f0b004858e7d959ca5a4
Jude Martin <jm129303@student.wpsok.org>
2025-11-05 13:16:09 -0600

    HW13

---
+++ HW13.py
@@ -0,0 +1,23 @@
+#Name:Jude Martin
+#Class: 5th Hour
+#Assignment: HW13
+
+#A common exercise in computer science is "FizzBuzz". The rules of
+#the game are simple. Count from 1 to 100 but with every number that
+#is divisible by 3 you say "Fizz" instead of the number,
+#every number divisible by 5 you say "Buzz" instead,
+#and if it's divisible by both you say "FizzBuzz".
+
+#Create a while loop that follows the rules of the game.
+
+i = 1
+while i <= 100:
+    if i % 3 == 0 and i % 5 == 0:
+        print ("Fizzbuzz")
+    elif i % 3 == 0:
+        print ("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print (i)
+    i += 1
