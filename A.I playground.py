4acc50ebe8fc7f6956e129b90484eb06ff3048eb
Jude Martin <jm129303@student.wpsok.org>
2025-11-14 13:10:32 -0600

    HW14

--- A.I playground.py
+++ A.I playground.py
@@ -1,51 +1,23 @@
+
+
+
 import pygame
+import random
 import sys
 
-# Initialize pygame
+# Initialize Pygame
 pygame.init()
 
-# Screen dimensions
-WIDTH, HEIGHT = 800, 600
+# Screen settings
+WIDTH, HEIGHT = 600, 800
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Super Mario Prototype")
+pygame.display.set_caption("Simple Shooter")
 
 # Colors
 WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
 RED = (255, 0, 0)
-YELLOW = (255, 255, 0)
+BLUE = (50, 150, 255)
+BLACK = (0, 0, 0)
 
-# FPS
-FPS = 60
-clock = pygame.time.Clock()
-
-# Tile size
-TILE_SIZE = 32
-
-# Define Player class
-class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__()
-        self.vel = None
-        self.image = pygame.Surface((TILE_SIZE, TILE_SIZE))
-        self.image.fill(BLUE)
-        self.rect = self.image.get_rect(topleft=(x, y))
-        self.velocity_x = 0
-        self.velocity_y = 0
-        self.speed = 5
-        self.gravity = 1
-        self.jump_power = -15
-        self.on_ground = False
-
-    def update(self, platforms):
-        # Horizontal movement
-        self.velocity_x = 0
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            self.velocity_x = -self.speed
-        if keys[pygame.K_RIGHT]:
-            var = self.vel
-
-
-
+# Player settings
+player_width, player_height = 50, 3
--- playground.py
+++ playground.py
@@ -1,34 +1,3 @@
-#name: Jude Martin
-#class: 5th hour
-#assighnment: playground
-import random
-
-
-
-
-
-players=["bryson" , "eli" , "aiden" , "alden" , "julian" , "ivan" , "huntar" , "statham" , "gabe"]
-bullets=[0,0,0,0,0,0,0,0,1]
-print(players)
-next_player=(random.choice(players))
-print(next_player)
-players.remove(next_player)
-print(players)
-print(bullets)
-next_bullet=(random.choice(bullets))
-print(next_bullet)
-bullets.remove(next_bullet)
-print(bullets)
-
-
-import random
-magic_8_ball=["yes","no","maybe","perchance","nuh uh monkey","hell no never in a million years","fine I guess"]
-random.shuffle(magic_8_ball)
-print(magic_8_ball)
-print(random.choice(magic_8_ball))
-
-
-
-
-
-
+i = "SIX SEVEN!!!!!!!!!!!!!!"
+while i != 0:
+    print (i)
